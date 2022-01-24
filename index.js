c5bcdff55b216c70970ce67f480b5dadd5f98d46
tolu <toluokupe@yahoo.com>
2022-01-24 04:32:04 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "Opx";
